8080a2cdd5a26caec31b79dfaf8d7e898949be1a
вашÐDimafdr <Dimafdr@yandex.ru>
2024-03-08 14:56:16 +0300

    modified:   server/settings.py

--- server/settings.py
+++ server/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
