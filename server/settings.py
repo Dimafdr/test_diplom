8c1f891487ff7a19c08af237be6d81237b5b26d9
вашÐDimafdr <Dimafdr@yandex.ru>
2024-03-31 12:33:24 +0300

    modified:   server/settings.py

--- server/settings.py
+++ server/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': '89.104.70.129',
+        'HOST': os.getenv('POSTGRES_HOST'),
         'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
